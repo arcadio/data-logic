7fba8bee67cf5b2b40155a24e563d05c8425bfe0
Arcadio Rubio García <arc@well.ox.ac.uk>
2022-08-30 15:54:30 +0200

    Uncomment constraints and add final newline

--- kb/constraints.pl
+++ kb/constraints.pl
@@ -1,15 +1,15 @@
 %
 % General Constraints
 %
-% constraint([every, exocrine_cell, secretes, a, enzyme]).
-% constraint([every, endocrine_cell, secretes, a, hormone]).
-% constraint([every, pancreatic_cell, part_of, pancreas]).
+constraint([every, exocrine_cell, secretes, a, enzyme]).
+constraint([every, endocrine_cell, secretes, a, hormone]).
+constraint([every, pancreatic_cell, part_of, pancreas]).
 
 
 %
 % Model-specific Constraints
 %
-% constraint([every, cell, which, part_for, pancreas, secretes, a, substance]).
-% constraint([pancreas, secretes, every, substance]).
-% constraint([every, endocrine_cell, which, isa, pancreatic_cell, part_for, endocrine_pancreas]).
-% constraint([every, exocrine_cell, isa, pancreatic_cell]).
+constraint([every, cell, which, part_for, pancreas, secretes, a, substance]).
+constraint([pancreas, secretes, every, substance]).
+constraint([every, endocrine_cell, which, isa, pancreatic_cell, part_for, endocrine_pancreas]).
+constraint([every, exocrine_cell, isa, pancreatic_cell]).
